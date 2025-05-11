7d50dcc981930bdc97802a91e7583efe95cdd504
Edu <eduardo.garay.2022@gmail.com>
2025-05-10 21:18:35 -0600

    dos fases de despliegue
    por algo se empieza, verdad?

---
+++ main.ps1
@@ -0,0 +1,35 @@
+#region Variables
+
+$phasesPath = Join-Path -Path $PSScriptRoot -ChildPath "phases"
+# Obtener todos los scripts de fase ordenados por nombre
+$phaseScripts = Get-ChildItem -Path $phasesPath -Filter *.ps1 | Sort-Object Name
+$menuOptions = @(
+    "1. Instalar Chocolatey (requiere reinicio)",
+    "2. Configurar red",
+    "3. ?",
+    "0. Salir"
+)
+
+#endregion
+
+Write-Host " ==== autobuild-a-server-automatically (asa) by: name ====" -ForegroundColor Cyan
+
+foreach ($option in $menuOptions) {
+    Write-Host $option
+}
+
+$_input = Read-Host -Prompt "Ingrese el número de la fase a ejecutar"
+$selectedPhase = ""
+
+switch ($_input) {
+    0 { clear; exit }
+    1 { $selectedPhase = "phase01.ps1" }
+    2 { $selectedPhase = "phase02.ps1" }
+    Default { Write-Host "Opcion no válida" }
+}
+
+foreach ($script in $phaseScripts) {
+    if ($script.Name -eq $selectedPhase) {
+        . $script.FullName
+    }
+}
---
+++ phases/phase02.ps1
@@ -0,0 +1,45 @@
+﻿function ConfigureAdapter {
+    Try {
+    Rename-NetAdapter -Name "Ethernet 2" -NewName "semita" -ErrorAction Stop
+    Rename-NetAdapter -Name "Ethernet" -NewName "default" -ErrorAction Stop
+
+    Write-Host "Se ha cambiado con exito el nombre de las interfaces!" -ForegroundColor Green
+    }
+    Catch {
+        Write-Host "Error al nombrar el adaptador:" -ForegroundColor Red
+        Write-Host $($_.Exception.Message) -ForegroundColor Yellow
+    }
+}
+
+function ConfigureIPAddress {
+    Try {
+        # IPv4
+        # New-NetIPAddress -IPAddress 172.30.64.1 -InterfaceAlias "default" -AddressFamily IPv4 -PrefixLength 20 -ErrorAction Stop | Out-Null
+    
+        New-NetIPAddress -IPAddress 172.16.0.4 -InterfaceAlias "semita" -AddressFamily IPv4 -PrefixLength 24 -ErrorAction Stop | Out-Null
+
+        Write-Host "Se ha cambiado con exito el subneteo de la red!" -ForegroundColor Green
+    }
+    Catch {
+        Write-Host "Error al configurar la IP:" -ForegroundColor Red
+        Write-Host $($_.Exception.Message) -ForegroundColor Yellow
+    }
+}
+
+function SetDNS {
+    Try {
+        
+        Set-DnsClientServerAddress -InterfaceAlias "semita" -ServerAddresses "172.16.0.2", "1.1.1.1" | Out-Null
+        Write-Host "Se ha asginado con exito el DNS en el servidor!" -ForegroundColor Green
+        ipconfig /flushdns | Out-Null
+        Write-Host "Nota: Se ha reiniciado el caché del DNS!" -ForegroundColor Cyan
+    }
+    Catch {
+        Write-Host "Error al asignar el DNS": -ForegroundColor Red
+        Write-Host $($_.Exception.Message) -ForegroundColor Yellow
+    }
+}
+
+ConfigureAdapter;
+ConfigureIPAddress;
+SetDNS;
